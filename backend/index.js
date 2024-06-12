e6fa68d04e535ff1357777122322699c48132eec
SimranArora1203 <113377852+SimranArora1203@users.noreply.github.com>
2024-06-12 16:15:02 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 app.use(cors());
 app.use(cors({
-  origin:[https://deploy-mern-1whq.vercel.app"],
+  origin:["https://deploy-mern-1whq.vercel.app"],
           methods:["POST","GET"],
           credentials:true
           }));
